5e725b32e1be3ad5bba4d6d27503bdd99a1eb489
p4cm4n972 <manuel.adele@gmail.com>
2016-12-27 18:25:13 +0100

    bien avancé

--- main.js
+++ main.js
@@ -1,25 +1,62 @@
-function draw() {
+'use strict';
+window.onload = function()
+{
     var canvas = document.getElementById('myCanvas');
-    if (canvas.getContext){
+    if (!canvas)
+    {
+        alert('impossible de recuperer le canvas');
+        return;
+    } 
     var ctx = canvas.getContext('2d');
-    
+    if(!ctx)
+    {
+        alert('impossible de recuperer le contexte');
+        return;
+    }
+    var myIntervalle = setInterval(animate, 1000/30);
 
+
+    //COORDONNEE SOLEIL
+    var diametreSoleil = 50;
+    var diametreAnneau = 70;
+    var xSoleil = 700+diametreSoleil/2;
+    var ySoleil = 20+diametreSoleil/2;
+    var vitesseX = 3 ;
+    var vitesseY = 3;
+
+
+    //FONCTION ANIMATION
+    function animate(){ 
+    //LE CIEL
+    ctx.beginPath();
     ctx.fillStyle = "rgb(119,181,254)";
     ctx.fillRect (0,0,720,250);
+    ctx.closePath();
+    
+
+    //LA PLAINE VERTE
+    ctx.beginPath();
     ctx.fillStyle = "rgb(0,255,0)";
     ctx.fillRect (0,250,720,110);
-    
-    var toRadians = Math.PI / 180; //CONVERTIR LES RADIANS (UNITES PAR DEFAUTS DE CANVAS) EN DEGRES
-    var endAngle = 360 * toRadians;
+    ctx.closePath();
+       
+    //LE SOLEIL
+    ctx.clearRect(xSoleil, ySoleil,diametreSoleil/2, 0, Math.PI*2);
+     
     ctx.beginPath();
+    ctx.arc(xSoleil, ySoleil, diametreSoleil/2, 0, Math.PI*2);
+    ctx.fill();
     ctx.fillStyle="rgb(255,255,0)";
-    ctx.moveTo(700, 20);
-    ctx.arc(700, 20, 50, 0, endAngle, false);
+    ctx.closePath();
+
+    xSoleil -= vitesseX;
+    ySoleil += vitesseY;
+
+   //ANNEAU DU SOLEIL
+   /* ctx.beginPath();
+    ctx.arc(xSoleil, ySoleil, diametreAnneau/2, 0, Math.PI*2);
     ctx.fill();
-    ctx.beginPath();
-    ctx.fillStyle="rgba(255,255,0,.5)";
-    ctx.moveTo(700, 20);
-    ctx.arc(700, 20, 70, 0, endAngle, false);
-    ctx.fill();
-    }
-}
+    ctx.fillStyle="rgba(255,255,0,.5)";*/
+    };
+};
+
